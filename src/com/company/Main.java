a0595b71a72d2de7e728c746e8920759167ac325
kris051d <kris051d@edu.easj.dk>
2021-09-08 12:57:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static double circleAre(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        circleAre();
+        System.out.println("boo!") + 7;.
+    }
+}
+// 4.1: Result of 'Main.circleAre()' is ignored
+// 4.2: Operator '+' cannot be applied to 'void', 'int'
